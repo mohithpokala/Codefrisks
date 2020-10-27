96d74267db30c5fc7dfa63d0c09af299cb5814a3
Pokala Mohith <62466438+mohithpokala@users.noreply.github.com>
2020-10-27 22:11:47 +0530

    Update settings.py

--- Codefrisk/settings.py
+++ Codefrisk/settings.py
@@ -32,7 +32,10 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'backend.apps.BackendConfig'
 ]
-CORS_ORIGIN_ALLOW_ALL=True
+CORS_ORIGIN_ALLOW_ALL=False
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200'
+]
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
