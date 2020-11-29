5c887f618bc1589e7221ef760d6638f60e137540
Pokala Mohith <62466438+mohithpokala@users.noreply.github.com>
2020-11-29 14:03:40 +0530

    Update serializers.py

--- backend/serializers.py
+++ backend/serializers.py
@@ -10,13 +10,14 @@ from django.http.response import JsonResponse
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
-        extra_kwargs={'password':{'write_only':True,'required':True}}
-    
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+    def create(self, validated_data):
+        user = User.objects.create_user(username=validated_data['username'],email=validated_data['email'],password=validated_data['password'])
+        
+        return user
 
 class DataSerializer(serializers.ModelSerializer):
     class Meta:
         model = add_data
         fields = "__all__"
-
-
